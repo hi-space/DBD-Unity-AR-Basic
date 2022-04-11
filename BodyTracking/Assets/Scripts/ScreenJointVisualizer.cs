e31358c4a9dd4a8f783f3d2e916435f4399837e0
hi-space <hispace.j@gmail.com>
2022-04-11 14:51:17 +0900

    2d Body tracking vertices

--- BodyTracking/Assets/Scripts/ScreenJointVisualizer.cs
+++ BodyTracking/Assets/Scripts/ScreenJointVisualizer.cs
@@ -7,153 +7,112 @@ using UnityEngine.XR.ARSubsystems;
 
 public class ScreenJointVisualizer : MonoBehaviour
 {
-    // enum JointIndices
-    // {
-    //     Invalid = -1,
-    //     Head = 0, // parent: Neck1 [1]
-    //     Neck1 = 1, // parent: Root [16]
-    //     RightShoulder1 = 2, // parent: Neck1 [1]
-    //     RightForearm = 3, // parent: RightShoulder1 [2]
-    //     RightHand = 4, // parent: RightForearm [3]
-    //     LeftShoulder1 = 5, // parent: Neck1 [1]
-    //     LeftForearm = 6, // parent: LeftShoulder1 [5]
-    //     LeftHand = 7, // parent: LeftForearm [6]
-    //     RightUpLeg = 8, // parent: Root [16]
-    //     RightLeg = 9, // parent: RightUpLeg [8]
-    //     RightFoot = 10, // parent: RightLeg [9]
-    //     LeftUpLeg = 11, // parent: Root [16]
-    //     LeftLeg = 12, // parent: LeftUpLeg [11]
-    //     LeftFoot = 13, // parent: LeftLeg [12]
-    //     RightEye = 14, // parent: Head [0]
-    //     LeftEye = 15, // parent: Head [0]
-    //     Root = 16, // parent: <none> [-1]
-    // }
-
     enum JointIndices
     {
         Invalid = -1,
-        Root = 0, // parent: <none> [-1]
-        Hips = 1, // parent: Root [0]
-        LeftUpLeg = 2, // parent: Hips [1]
-        LeftLeg = 3, // parent: LeftUpLeg [2]
-        LeftFoot = 4, // parent: LeftLeg [3]
-        LeftToes = 5, // parent: LeftFoot [4]
-        LeftToesEnd = 6, // parent: LeftToes [5]
-        RightUpLeg = 7, // parent: Hips [1]
-        RightLeg = 8, // parent: RightUpLeg [7]
-        RightFoot = 9, // parent: RightLeg [8]
-        RightToes = 10, // parent: RightFoot [9]
-        RightToesEnd = 11, // parent: RightToes [10]
-        Spine1 = 12, // parent: Hips [1]
-        Spine2 = 13, // parent: Spine1 [12]
-        Spine3 = 14, // parent: Spine2 [13]
-        Spine4 = 15, // parent: Spine3 [14]
-        Spine5 = 16, // parent: Spine4 [15]
-        Spine6 = 17, // parent: Spine5 [16]
-        Spine7 = 18, // parent: Spine6 [17]
-        LeftShoulder1 = 19, // parent: Spine7 [18]
-        LeftArm = 20, // parent: LeftShoulder1 [19]
-        LeftForearm = 21, // parent: LeftArm [20]
-        LeftHand = 22, // parent: LeftForearm [21]
-        LeftHandIndexStart = 23, // parent: LeftHand [22]
-        LeftHandIndex1 = 24, // parent: LeftHandIndexStart [23]
-        LeftHandIndex2 = 25, // parent: LeftHandIndex1 [24]
-        LeftHandIndex3 = 26, // parent: LeftHandIndex2 [25]
-        LeftHandIndexEnd = 27, // parent: LeftHandIndex3 [26]
-        LeftHandMidStart = 28, // parent: LeftHand [22]
-        LeftHandMid1 = 29, // parent: LeftHandMidStart [28]
-        LeftHandMid2 = 30, // parent: LeftHandMid1 [29]
-        LeftHandMid3 = 31, // parent: LeftHandMid2 [30]
-        LeftHandMidEnd = 32, // parent: LeftHandMid3 [31]
-        LeftHandPinkyStart = 33, // parent: LeftHand [22]
-        LeftHandPinky1 = 34, // parent: LeftHandPinkyStart [33]
-        LeftHandPinky2 = 35, // parent: LeftHandPinky1 [34]
-        LeftHandPinky3 = 36, // parent: LeftHandPinky2 [35]
-        LeftHandPinkyEnd = 37, // parent: LeftHandPinky3 [36]
-        LeftHandRingStart = 38, // parent: LeftHand [22]
-        LeftHandRing1 = 39, // parent: LeftHandRingStart [38]
-        LeftHandRing2 = 40, // parent: LeftHandRing1 [39]
-        LeftHandRing3 = 41, // parent: LeftHandRing2 [40]
-        LeftHandRingEnd = 42, // parent: LeftHandRing3 [41]
-        LeftHandThumbStart = 43, // parent: LeftHand [22]
-        LeftHandThumb1 = 44, // parent: LeftHandThumbStart [43]
-        LeftHandThumb2 = 45, // parent: LeftHandThumb1 [44]
-        LeftHandThumbEnd = 46, // parent: LeftHandThumb2 [45]
-        Neck1 = 47, // parent: Spine7 [18]
-        Neck2 = 48, // parent: Neck1 [47]
-        Neck3 = 49, // parent: Neck2 [48]
-        Neck4 = 50, // parent: Neck3 [49]
-        Head = 51, // parent: Neck4 [50]
-        Jaw = 52, // parent: Head [51]
-        Chin = 53, // parent: Jaw [52]
-        LeftEye = 54, // parent: Head [51]
-        LeftEyeLowerLid = 55, // parent: LeftEye [54]
-        LeftEyeUpperLid = 56, // parent: LeftEye [54]
-        LeftEyeball = 57, // parent: LeftEye [54]
-        Nose = 58, // parent: Head [51]
-        RightEye = 59, // parent: Head [51]
-        RightEyeLowerLid = 60, // parent: RightEye [59]
-        RightEyeUpperLid = 61, // parent: RightEye [59]
-        RightEyeball = 62, // parent: RightEye [59]
-        RightShoulder1 = 63, // parent: Spine7 [18]
-        RightArm = 64, // parent: RightShoulder1 [63]
-        RightForearm = 65, // parent: RightArm [64]
-        RightHand = 66, // parent: RightForearm [65]
-        RightHandIndexStart = 67, // parent: RightHand [66]
-        RightHandIndex1 = 68, // parent: RightHandIndexStart [67]
-        RightHandIndex2 = 69, // parent: RightHandIndex1 [68]
-        RightHandIndex3 = 70, // parent: RightHandIndex2 [69]
-        RightHandIndexEnd = 71, // parent: RightHandIndex3 [70]
-        RightHandMidStart = 72, // parent: RightHand [66]
-        RightHandMid1 = 73, // parent: RightHandMidStart [72]
-        RightHandMid2 = 74, // parent: RightHandMid1 [73]
-        RightHandMid3 = 75, // parent: RightHandMid2 [74]
-        RightHandMidEnd = 76, // parent: RightHandMid3 [75]
-        RightHandPinkyStart = 77, // parent: RightHand [66]
-        RightHandPinky1 = 78, // parent: RightHandPinkyStart [77]
-        RightHandPinky2 = 79, // parent: RightHandPinky1 [78]
-        RightHandPinky3 = 80, // parent: RightHandPinky2 [79]
-        RightHandPinkyEnd = 81, // parent: RightHandPinky3 [80]
-        RightHandRingStart = 82, // parent: RightHand [66]
-        RightHandRing1 = 83, // parent: RightHandRingStart [82]
-        RightHandRing2 = 84, // parent: RightHandRing1 [83]
-        RightHandRing3 = 85, // parent: RightHandRing2 [84]
-        RightHandRingEnd = 86, // parent: RightHandRing3 [85]
-        RightHandThumbStart = 87, // parent: RightHand [66]
-        RightHandThumb1 = 88, // parent: RightHandThumbStart [87]
-        RightHandThumb2 = 89, // parent: RightHandThumb1 [88]
-        RightHandThumbEnd = 90, // parent: RightHandThumb2 [89]
+        Head = 0, // parent: Neck1 [1]
+        Neck1 = 1, // parent: Root [16]
+        RightShoulder1 = 2, // parent: Neck1 [1]
+        RightForearm = 3, // parent: RightShoulder1 [2]
+        RightHand = 4, // parent: RightForearm [3]
+        LeftShoulder1 = 5, // parent: Neck1 [1]
+        LeftForearm = 6, // parent: LeftShoulder1 [5]
+        LeftHand = 7, // parent: LeftForearm [6]
+        RightUpLeg = 8, // parent: Root [16]
+        RightLeg = 9, // parent: RightUpLeg [8]
+        RightFoot = 10, // parent: RightLeg [9]
+        LeftUpLeg = 11, // parent: Root [16]
+        LeftLeg = 12, // parent: LeftUpLeg [11]
+        LeftFoot = 13, // parent: LeftLeg [12]
+        RightEye = 14, // parent: Head [0]
+        LeftEye = 15, // parent: Head [0]
+        Root = 16, // parent: <none> [-1]
     }
 
     [SerializeField]
-    [Tooltip("The AR camera being used in the scene.")]
-    Camera m_ARCamera;
+    Camera arCamera;
+    ARHumanBodyManager humanBodyManager;
 
-    [Tooltip("The ARHumanBodyManager which will produce human body anchors.")]
-    ARHumanBodyManager m_HumanBodyManager;
-
+    // lines
     [SerializeField]
-    GameObject m_LineRendererPrefab;
+    GameObject linePrefab;
 
-    Dictionary<int, GameObject> m_LineRenderers;
+    Dictionary<int, GameObject> lineRenderers;
     static HashSet<int> s_JointSet = new HashSet<int>();
+
+    // vertices
+    [SerializeField]
+    GameObject vertexPrefab;
+
+    Dictionary<int, GameObject> vertexObjects;
 
     void Awake()
     {
-        m_HumanBodyManager = (ARHumanBodyManager) GetComponent<ARHumanBodyManager>();
-        m_LineRenderers = new Dictionary<int, GameObject>();
+        humanBodyManager = (ARHumanBodyManager) GetComponent<ARHumanBodyManager>();
+        lineRenderers = new Dictionary<int, GameObject>();
+        vertexObjects = new Dictionary<int, GameObject>();
     }
 
+    void Update()
+    {
+        NativeArray<XRHumanBodyPose2DJoint> joints = humanBodyManager.GetHumanBodyPose2DJoints(Allocator.Temp);
+
+        // if (joints.IsCreated)
+        // {
+        //     UpdateVertices(joints);
+        // }
+
+        if (!joints.IsCreated)
+        {
+            HideJointLines();
+            return;
+        }
+        
+        UpdateVertices(joints);
+        s_JointSet.Clear();
+        for (int i = joints.Length - 1; i >=0; --i) 
+        {
+            if (joints[i].parentIndex != -1) 
+            {
+                UpdateRenderer(joints, i);
+            }
+        }
+    }
+
+    void UpdateVertices(NativeArray<XRHumanBodyPose2DJoint> joints)
+    {
+        for (int index = 0; index < joints.Length; index++)
+        {
+            XRHumanBodyPose2DJoint joint = joints[index];
+
+            GameObject vertexObject;
+            if (!vertexObjects.TryGetValue(index, out vertexObject))
+            {
+                vertexObject = Instantiate(vertexPrefab, transform);
+                vertexObjects.Add(index, vertexObject);
+            }
+
+            if (joint.tracked)
+            {
+                vertexObject.transform.position = arCamera.ViewportToWorldPoint(
+                    new Vector3(joint.position.x, joint.position.y, 2.0f)); // world position
+            }
+            else 
+            {
+                vertexObject.SetActive(false);
+            }
+        }
+    }
     void UpdateRenderer(NativeArray<XRHumanBodyPose2DJoint> joints, int index)
     {
-        GameObject lineRendererGO;
-        if (!m_LineRenderers.TryGetValue(index, out lineRendererGO))
+        GameObject lineObject;
+        if (!lineRenderers.TryGetValue(index, out lineObject))
         {
-            lineRendererGO = Instantiate(m_LineRendererPrefab, transform);
-            m_LineRenderers.Add(index, lineRendererGO);
+            lineObject = Instantiate(linePrefab, transform);
+            lineRenderers.Add(index, lineObject);
         }
 
-        var lineRenderer = lineRendererGO.GetComponent<LineRenderer>();
+        var lineRenderer = lineObject.GetComponent<LineRenderer>();
 
         // Traverse hierarchy to determine the longest line set that needs to be drawn.
         var positions = new NativeArray<Vector2>(joints.Length, Allocator.Temp);
@@ -164,9 +123,7 @@ public class ScreenJointVisualizer : MonoBehaviour
             while (boneIndex >= 0)
             {
                 var joint = joints[boneIndex];
-                    
-                Debug.Log("=====>" + boneIndex);
-                Debug.Log(joints[boneIndex]);
+
                 if (joint.tracked)
                 {
                     positions[jointCount++] = joint.position;
@@ -186,11 +143,11 @@ public class ScreenJointVisualizer : MonoBehaviour
             for (int i = 0; i < jointCount; ++i)
             {
                 var position = positions[i];
-                var worldPosition = m_ARCamera.ViewportToWorldPoint(
-                    new Vector3(position.x, position.y, m_ARCamera.nearClipPlane));
+                var worldPosition = arCamera.ViewportToWorldPoint(
+                    new Vector3(position.x, position.y, arCamera.nearClipPlane));
                 lineRenderer.SetPosition(i, worldPosition);
             }
-            lineRendererGO.SetActive(true);
+            lineObject.SetActive(true);
         }
         finally
         {
@@ -198,30 +155,9 @@ public class ScreenJointVisualizer : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        Debug.Assert(m_HumanBodyManager != null, "Human body manager cannot be null");
-        var joints = m_HumanBodyManager.GetHumanBodyPose2DJoints(Allocator.Temp);
-        if (!joints.IsCreated)
-        {
-            HideJointLines();
-            return;
-        }
-
-        using (joints)
-        {
-            s_JointSet.Clear();
-            for (int i = joints.Length - 1; i >= 0; --i)
-            {
-                if (joints[i].parentIndex != -1)
-                    UpdateRenderer(joints, i);
-            }
-        }
-    }
-
     void HideJointLines()
     {
-        foreach (var lineRenderer in m_LineRenderers)
+        foreach (var lineRenderer in lineRenderers)
         {
             lineRenderer.Value.SetActive(false);
         }
